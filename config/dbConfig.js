1656e09c1720f8c139fae4f4dd3b1dba911157ef
TariroChimutopo <tariro@qubedlab.com>
2022-06-30 22:30:37 +0200

    new database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'notes',
-    dialect: 'mysql',
+  HOST: "sql5.freemysqlhosting.net",
+  USER: "sql5503234",
+  PASSWORD: "qVNYtSR4Ql",
+  DB: "sql5503234",
+  dialect: "mysql",
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquired: 30000,
-        idle: 1000
-    }
-}
+  pool: {
+    max: 5,
+    min: 0,
+    acquired: 30000,
+    idle: 1000,
+  },
+};
